75a8787a5f2d854f09db165219d0849fe2ddb629
David Lin <davxlin@gmail.com>
2020-02-03 22:43:29 -0800

    issue #1

---
+++ app/src/main/java/com/example/splitter/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.splitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // widgets
+    EditText names;
+    EditText tip;
+    EditText subtotal;
+    TextView name_list;
+    TextView bill_params;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ArrayList<String> people = new ArrayList<>();
+
+        names = findViewById(R.id.names);
+        name_list = findViewById(R.id.name_list);
+        tip = findViewById(R.id.tip);
+        subtotal = findViewById(R.id.subtotal);
+        bill_params = findViewById(R.id.bill_params);
+        final FloatingActionButton next = findViewById(R.id.next_activity);
+        final Button addPerson = findViewById(R.id.add_person);
+
+        addPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = names.getText().toString();
+                if (name.length() == 0) {
+                    // received an empty name. Should pop-up an error dialog and not add to list
+                    // Use the AlertDialog.Builder to configure the AlertDialog.
+                    final AlertDialog.Builder alertDialogBuilder =
+                            new AlertDialog.Builder(view.getContext())
+                                    .setTitle("Blank Name")
+                                    .setMessage("Names cannot be empty!")
+                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    })
+                                    ;
+
+// Show the AlertDialog.
+                    alertDialogBuilder.show();
+                } else {
+                    people.add(name);
+                    name_list.append(name + "\n");
+                    names.setText("");
+                }
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), ItemizeActivity.class);
+                intent.putExtra("subtotal", Double.parseDouble(subtotal.getText().toString()));
+                intent.putExtra("tip", Integer.parseInt(tip.getText().toString()));
+                intent.putExtra("names", people);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
